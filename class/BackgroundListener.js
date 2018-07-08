3c67e5e4e9d1c42a5e28b7c21ff410ac9e4b1da5
livnoni <yehuda.livnoni@gmail.com>
2018-07-08 20:36:16 +0300

    added keyboardListener() and touchListener() functions.

--- class/BackgroundListener.js
+++ class/BackgroundListener.js
@@ -1,25 +1,37 @@
 class BackgroundListener {
     constructor(sound) {
+        this.sound = sound;
+
+        this.keyboardListener();
+        this.touchListener();
+
+    }
+
+    keyboardListener() {
         document.addEventListener("keydown", direction);
+
         var self = this;
 
         function direction(event) {
 
             if ((event.keyCode == 37 || event.keyCode == 65) && snakeDirection != "RIGHT") {
-                move("left");
+                self.move("left");
             } else if ((event.keyCode == 38 || event.keyCode == 87) && snakeDirection != "DOWN") {
-                move("up");
+                self.move("up");
             } else if ((event.keyCode == 39 || event.keyCode == 68) && snakeDirection != "LEFT") {
-                move("right");
+                self.move("right");
             } else if ((event.keyCode == 40 || event.keyCode == 83) && snakeDirection != "UP") {
-                move("down");
+                self.move("down");
             }
         }
+    }
 
-
+    touchListener() {
         //this for mobiles:
         document.addEventListener('touchstart', handleTouchStart, false);
         document.addEventListener('touchmove', handleTouchMove, false);
+
+        var self = this;
 
         var xDown = null;
         var yDown = null;
@@ -30,7 +42,7 @@ class BackgroundListener {
         };
 
         function handleTouchMove(evt) {
-            if ( ! xDown || ! yDown ) {
+            if (!xDown || !yDown) {
                 return;
             }
 
@@ -40,43 +52,34 @@ class BackgroundListener {
             var xDiff = xDown - xUp;
             var yDiff = yDown - yUp;
 
-            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-                if ( xDiff > 0 ) {
+            if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+                if (xDiff > 0) {
                     /* left swipe */
-                    move("left");
+                    self.move("left");
                 } else {
                     /* right swipe */
-                    move("right");
+                    self.move("right");
                 }
             } else {
-                if ( yDiff > 0 ) {
+                if (yDiff > 0) {
                     /* up swipe */
-                    move("up");
+                    self.move("up");
                 } else {
                     /* down swipe */
-                    move("down");
+                    self.move("down");
                 }
             }
             /* reset values */
             xDown = null;
             yDown = null;
-        };
-
-        function move(direction){
-            snakeDirection = direction.toUpperCase();
-            self.toString();
-            sound.play(direction.toLowerCase());
         }
-
-
     }
 
-
-
-
-
-
-
+    move(direction) {
+        snakeDirection = direction.toUpperCase();
+        this.toString();
+        this.sound.play(direction.toLowerCase());
+    }
 
     toString() {
         console.log(`direction changed to -> ${snakeDirection}`);
