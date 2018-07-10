34b0f8ec67a1695987900f759a215416217908bc
livnoni <yehuda.livnoni@gmail.com>
2018-07-11 01:19:37 +0300

    added post data to mongo DB

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
+var MongoClient = require('mongodb').MongoClient;
+
+function writeToDB(obj, collectionName){
+    MongoClient.connect(process.env.mongoUrl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("snakeGame");
+        dbo.collection(collectionName).insertOne(obj, function(err, res) {
+            if (err) throw err;
+            console.log(`1 document inserted -> [${JSON.stringify(obj)}]`);
+            db.close();
+        });
+    });
+}
+
 
 app.get('/score', function (req, res) {
     console.log("got /score request.");
@@ -18,8 +32,15 @@ app.post('/sendScore', function(req, res) {
     console.log("got post sendScore");
     console.log("req.body=",req.body);
 
+
     var name = req.body.name;
     var score = req.body.score;
+
+    writeToDB({name , score}, "scores");
+
+
+    //send to DB:
+
 
     res.send("succsefully got score="+score+" to "+name);
 });
