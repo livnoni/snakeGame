5563acea69319448d490ee369cfcacd7bf3e0262
livnoni <yehuda.livnoni@gmail.com>
2018-07-11 02:46:15 +0300

    added to onGameOver function request to node server with the game data

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-function onGameOver() {
+function onGameOver(data) {
+    var username;
+    var username = prompt("Please enter your name:", "your name");
+    if (username == null || username == "") {
+        console.log("User cancelled the prompt.")
+    } else {
+        console.log("username=",username)
+        console.log("data=",Object.assign(data,{name:username}));
+
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://livnonisnake.herokuapp.com/sendScore", true);
+    xhr.open("POST", "http://localhost:5000/sendScore", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+
+
+
     if (confirm("Do you want to play again ?")) {
         var game = new Game(config.soundPath, new Snake());
         console.log("game=\n"+JSON.stringify(game));
