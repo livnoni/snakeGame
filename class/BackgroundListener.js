02549ff25d34fd32f874fd5d22167814948faa37
livnoni <yehuda.livnoni@gmail.com>
2018-07-12 23:09:36 +0300

    handle sutuation playe doent write his name

--- class/BackgroundListener.js
+++ class/BackgroundListener.js
@@ -143,7 +143,7 @@ class BackgroundListener {
         xmlHttp.onreadystatechange = function() {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
                 callback(xmlHttp.responseText);
-        }
+        };
         xmlHttp.open("GET", theUrl, true); // true for asynchronous
         xmlHttp.send(null);
     }
