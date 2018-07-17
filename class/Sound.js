6097e643e5b92b4dc5637f3fe6cf4ef064c61fd8
livnoni <yehuda.livnoni@gmail.com>
2018-07-17 23:27:05 +0300

    add to do

--- class/Sound.js
+++ class/Sound.js
@@ -1,11 +1,11 @@
-//todo: what is the name of this pattern?
+//todo: what is the name of this pattern? (Factory)
 
 class Sound {
-    constructor(soundPath) {
+    constructor(soundPaths) {
         var counter = 0;
-        for (var soundName in soundPath) {
+        for (var soundName in soundPaths) {
             counter++;
-            this[soundName] = this._createAudioFile(soundPath[soundName]);
+            this[soundName] = this._createAudioFile(soundPaths[soundName]);
             console.log(`created new sound of ${soundName}.`);
         }
         console.log(`Audio class have created successfully ${counter} sounds.`);
